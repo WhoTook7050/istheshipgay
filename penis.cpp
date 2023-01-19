fff2844b0ca9d0adc713394db07c0dea51c64c02
Josh <92704148+WhoTook7050@users.noreply.github.com>
2023-01-19 10:23:26 -0600

    Create penis.cpp

---
+++ penis.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string.h>
+//#include <time.h>
+
+//fuck you database
+
+//what if we include it twice??
+
+#include <time.h>
+#include <time.h>
+
+std::string tw = "🇨🇳";
+std::string cn = "🇨🇳";
+
+//void 🔥🔥🔥🔥🔥🔥();
+//{
+//
+//}
+
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+
+int checkindependance🇨🇳()
+{
+
+    if (tw == cn) {
+        return 0;
+        }
+    else {
+        std::cout << "VIOLATION OF CHINESE COMMUNIST PARTY POLICY 违反中国共产党的政策";
+    }
+}
+
+int main()
+
+{
+
+    checkindependance🇨🇳;
+
+    auto& 库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳= std::cout;
+    auto& 水稻收成_rice_harvest = std::cin;
+
+    typedef std::string 维吾尔自治区_uyghur_autonomous_region;
+
+    维吾尔自治区_uyghur_autonomous_region the;
+    维吾尔自治区_uyghur_autonomousregion the;
+
+    库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "enter first character\n";
+    水稻收成_rice_harvest >> the;
+
+    库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "\n";
+    库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "enter second character\n";
+    水稻收成_riceharvest >> the;
+
+    /std::cout << waifu1 << " ";
+    std::cout << waifu2;/
+
+    库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "you are gay";
+
+}
