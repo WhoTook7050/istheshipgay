0ddb5f534e643c79ed2e1b1c0f1290728992feba
nepnep <119973523+nepnep39@users.noreply.github.com>
2023-01-19 11:45:59 -0500

    Update and rename penis.cpp to nepnepdidallthework.cpp
    new and improved code

---
+++ nepnepdidallthework.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string.h>
+//#include <time.h>
+
+//fuck you database
+
+//what if we include it twice??
+
+#include <time.h>
+#include <time.h>
+
+std::string tw = "🇨🇳";
+std::string cn = "🇨🇳";
+
+//void 🔥🔥🔥🔥🔥🔥();
+//{
+//
+//}
+
+//lol why is it toso's repo i wrote all the code
+//do not let him lie to you, this was his ideas
+
+//
+//
+
+
+
+
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+using namespace std;
+
+int check_independance_🇨🇳()
+{
+
+	if (tw == cn) {
+		return 0;
+		}
+	else {
+		std::cout << "VIOLATION OF CHINESE COMMUNIST PARTY POLICY 违反中国共产党的政策";
+	}
+}
+
+int main()
+
+{
+
+
+
+
+
+	//whitespace lol
+
+
+
+	check_independance_🇨🇳;
+
+	auto& 库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳= std::cout;
+	auto& 水稻收成_rice_harvest = std::cin;
+
+	typedef std::string 维吾尔自治区_uyghur_autonomous_region;
+
+	维吾尔自治区_uyghur_autonomous_region the;
+	维吾尔自治区_uyghur_autonomous_region the_;
+
+	库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "enter first character\n";
+	水稻收成_rice_harvest >> the;
+
+	库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "\n";
+	库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "enter second character\n";
+	水稻收成_rice_harvest >> the_;
+
+	/*std::cout << waifu1 << " ";
+	std::cout << waifu2;*/
+
+	库特🔥🔥🔥🇨🇳🇨🇳🇨🇳🇨🇳 << "you are gay";
+
+}
